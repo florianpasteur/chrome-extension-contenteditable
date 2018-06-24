ee661142961ed989c91f2097d377c2359089fad4
Vadim Makeev <pepelsbey@gmail.com>
2018-06-24 16:56:03 +0300

    Replace tabs with spaces

--- background.js
+++ background.js
@@ -1,33 +1,33 @@
 var contentEditable = false;
 
 function enable(tabId, enable) {
-	var icon = enable ? '-active' : '';
-	var title = enable ? 'disable' : 'enable';
+    var icon = enable ? '-active' : '';
+    var title = enable ? 'disable' : 'enable';
 
-	chrome.pageAction.setIcon({
-		path: {
-			'19': 'images/button-19' + icon + '.png',
-			'38': 'images/button-38' + icon + '.png'
-		},
-		tabId: tabId
-	});
+    chrome.pageAction.setIcon({
+        path: {
+            '19': 'images/button-19' + icon + '.png',
+            '38': 'images/button-38' + icon + '.png'
+        },
+        tabId: tabId
+    });
 
-	chrome.pageAction.setTitle({
-		title: chrome.i18n.getMessage(title),
-		tabId: tabId
-	});
+    chrome.pageAction.setTitle({
+        title: chrome.i18n.getMessage(title),
+        tabId: tabId
+    });
 
-	chrome.tabs.executeScript(tabId, {
-		code: 'document.body.setAttribute("contenteditable", "' + enable + '")'
-	});
+    chrome.tabs.executeScript(tabId, {
+        code: 'document.body.setAttribute("contenteditable", "' + enable + '")'
+    });
 
-	contentEditable = enable;
+    contentEditable = enable;
 }
 
 chrome.tabs.onUpdated.addListener(function(tab) {
-	chrome.pageAction.show(tab);
+    chrome.pageAction.show(tab);
 });
 
 chrome.pageAction.onClicked.addListener(function(tab) {
-	enable(tab.id, !contentEditable);
+    enable(tab.id, !contentEditable);
 });
